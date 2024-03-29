8b8950595fd6baa4ece8abf79023125269509f29
mzeballosn <mzeballon@gmail.com>
2024-03-29 12:11:57 -0400

    server basico - docker

---
+++ api/index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+
+const app = express();
+
+
+app.use('/',(req,res)=>{
+    res.send('my app express')
+})
+
+app.listen(3020,()=>{
+    console.log('server listen port 3020')
+})
